19807a121f39c1ae10c48ff16ab32f19f639dad5
Dorgee Lama <DorgeeLama@exactone.com>
2022-01-10 12:10:52 -0700

    routes created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect to Database
+connectDB();
 
 // this is going to look for an enviornment variable called port to use if not found will use port 5000
 const PORT = process.env.PORT || 5000;
@@ -8,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('API running');
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
